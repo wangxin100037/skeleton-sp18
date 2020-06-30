f32429807f506cdefeaeb5a7203b43457b61880c
wangxin100037 <xinw@lyft.com>
2020-06-29 21:53:27 -0700

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,17 @@
+public class HelloNumbers{
+    public static void main(String[] args) {
+        int x;
+        x =0;
+        while(x <10){
+            System.out.println(x);
+            x = x+1;
+        }
+    }
+}
+
+/*
+1. before java variables be used, it has to be decalred
+2. java variables must be a specific type
+3. java variable types can never be changed
+4. types are verified before the code even runs (when compile?)
+*/ 
